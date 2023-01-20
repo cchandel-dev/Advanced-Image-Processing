6d326282c770da3fac82eb22d248d76ffa0a56e2
Chandan <cchandel@uwo.ca>
2023-01-20 17:49:38 -0500

    restructured image read in

--- SIFT Algorithm/Assignement1.py
+++ SIFT Algorithm/Assignement1.py
@@ -1,30 +1,46 @@
 import os
 import cv2
-import subprocess
+import Sift
+import random
+
 #load image dataset from input file
-directory = "input"
-images = []
+directory = "input\Dog"
+X = []
+Y = []
 for filename in os.listdir(directory):
     # load the image file using opencv
     img = cv2.imread(os.path.join(directory, filename))
     # append the image to the list
-    images.append(img)
+    X.append(img)
+    Y.append(1)
+#load image dataset from input file
+directory = "input\Cat"
+for filename in os.listdir(directory):
+    # load the image file using opencv
+    img = cv2.imread(os.path.join(directory, filename))
+    # append the image to the list
+    X.append(img)
+    Y.append(0)
+
+# shuffle the data
+data = list(zip(X, Y))
+random.shuffle(data)
+X, Y = zip(*data)
+
+# split the data
+split = int(0.8 * len(X))
+X_train, X_test = X[:split], X[split:]
+Y_train, Y_test = Y[:split], Y[split:]
 
 #PATH 1
-#run image dataset through sift feature extractor
-i = 0
-for image in images:
-    subprocess.run(["python3.10", "Sift.py", i])
-    i+=1
+#run the entire input dataset through sift feature extractor
+X_train_sift = []
+for img in X_train:
+    X_train_sift.append(Sift.get_kp(img))
 
-#load image dataset from output file
-directory = "output"
-siftimages = []
-for filename in os.listdir(directory):
-    # load the image file using opencv
-    img = cv2.imread(os.path.join(directory, filename))
-    # append the image to the list
-    siftimages.append(img)
+X_test_sift = []
+for img in X_test:
+    X_test_sift.append(Sift.get_kp(img))
 
 #feed to classifying neural network
 
